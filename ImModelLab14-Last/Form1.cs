4683d115b13e449c224633de0a830053b215e1c9
Natasha-Khaustova <45619280+Natasha-Khaustova@users.noreply.github.com>
2023-06-08 01:15:30 +0700

    KHAUSTOVA
    LAB14

---
+++ ImModelLab14-Last/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImModelLab14_Last
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int BusyOperators = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            chart1.Series[0].Points.Clear();
+            Model.Run((double)ArrivalEd.Value, (double)ParamOfServEd.Value, (int)NumOfOperEd.Value);
+            chart1.Series[0].Points.AddXY(0, 0);
+            BusyOperators = Model.getBusyOperatorsSize();
+            timer1.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            timer1.Enabled = Model.Iter();
+            if (BusyOperators < Model.getBusyOperatorsSize())
+            {
+                chart1.Series[0].Points.AddXY(Model.Time, BusyOperators);
+                BusyOperators++;
+                chart1.Series[0].Points.AddXY(Model.Time, BusyOperators);
+            }
+            else
+            {
+                chart1.Series[0].Points.AddXY(Model.Time, BusyOperators);
+                BusyOperators--;
+                chart1.Series[0].Points.AddXY(Model.Time, BusyOperators);
+            }
+            PrQueLbl.Text = Model.queueSize().ToString();
+            NonPrQueLbl.Text = Model.queueSizeNonPr().ToString();
+            AllCustomLbl.Text = Model.AllCustomersAmount.ToString();
+            LeftCustomLbl.Text = Model.getLeftCustom().ToString();
+        }
+    }
+}
---
+++ ImModelLab14-Last/Model.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImModelLab14_Last
+{
+    public static class Model
+    {
+        private static List<Agent> agents = new List<Agent>();
+        private static System system;
+        private static ArrivalProcess arrivalProcess;
+        private static double T = 0;
+        public static double Time { get { return T; } }
+        private static Agent activeAgent;
+        private static int CustomersAmount;
+        public static int AllCustomersAmount { get { return CustomersAmount; } }
+        private static double LambdaArrival = 0;
+        public static double LArr { get { return LambdaArrival; } }
+        private static double LambdaOperator = 0;
+        private static double LambdaOperatorNonPr = 0;
+        public static double LOper { get { return LambdaOperator; } }
+        public static double LOperNonPr { get { return LambdaOperatorNonPr; } }
+        private static int OperatorsAmount = 0;
+        public static int OpsAmount { get { return OperatorsAmount; } }
+
+        public static void Run(double LambdaArr, double lambdaOper, int opsAmount)
+        {
+            LambdaArrival = LambdaArr;
+            LambdaOperator = lambdaOper;
+            OperatorsAmount = opsAmount;
+            system = new System();
+            arrivalProcess = new ArrivalProcess(system);
+            agents.Add(system);
+            agents.Add(arrivalProcess);
+            T = 0;
+        }
+        public static bool Iter()
+        {
+            if (continueCondition(T, activeAgent))
+            {
+                double tMin = double.MaxValue;
+                activeAgent = null;
+                foreach (Agent agent in agents)
+                {
+                    double tAgent = agent.getNextEventTime();
+                    if (tAgent < tMin)
+                    {
+                        tMin = tAgent;
+                        activeAgent = agent;
+                    }
+                }
+                T = tMin;
+                if (activeAgent != null) activeAgent.processEvent();
+                CustomersAmount++;
+                return true;
+            }
+            return false;
+        }
+        public static bool continueCondition(double t, Agent activeAgent)
+        {
+            return (t < 100);
+        }
+        public static int queueSize()
+        {
+            return system.getQueueSize();
+        }
+        public static int queueSizeNonPr()
+        {
+            return system.getQueueSizeNonPr();
+        }
+        public static int getLeftCustom()
+        {
+            return system.leaveCustom;
+        }
+        public static int getBusyOperatorsSize()
+        {
+            return system.getBusyOperatorsSize();
+        }
+    }
+}
